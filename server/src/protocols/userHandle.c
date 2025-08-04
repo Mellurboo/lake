68c1b8dc8c3a5a274011eea47f4b86d2e77fc587
DcraftBg <czl_obshta@abv.bg>
2025-08-04 16:34:25 +0300

    [server] userHandle

--- server/src/protocols/userHandle.c
+++ server/src/protocols/userHandle.c
@@ -6,6 +6,7 @@
 #include "db_context.h"
 #include "error.h"
 
+extern DbContext* db;
 void userGetUserIdFromHandle(Client* client, Request* header) {
     char handle[MAX_HANDLE_SIZE];
     if(header->packet_len > MAX_HANDLE_SIZE || header->packet_len == 0) {
@@ -16,7 +17,21 @@ void userGetUserIdFromHandle(Client* client, Request* header) {
     intptr_t e = client_read(client, handle, header->packet_len);
     if(e <= 0) return;
 
-
+    uint32_t id;
+    e = DbContext_get_user_id_from_handle(db, handle, header->packet_len, &id);
+    if(e < 0) {
+        client_write_error(client, header->packet_id, ERROR_DB);
+        return;
+    }
+    Response response = {
+        .packet_id = header->packet_id,
+        .opcode = 0,
+        .packet_len = sizeof(id),
+    };
+    response_hton(&response);
+    e = client_write(client, &response, sizeof(response));
+    if(e <= 0) return;
+    client_write(client, &id, sizeof(id));
 }
 
 protocol_func_t userHandleProtocolFuncs[] = {
